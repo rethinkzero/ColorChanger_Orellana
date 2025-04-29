785dac56669363ac07ef547772f1c17c40a83bca
rethinkzero <93017797+rethinkzero@users.noreply.github.com>
2025-04-29 12:21:41 -0400

    Update color.js

--- color.js
+++ color.js
@@ -1,1 +1,8 @@
+let button = document.querySelector("#changer");
+let box = document.querySelector("#boxer");
 
+button.addEventListener("click", function() {
+  // code to change the color goes here
+});
+
+box.style.backgroundColor = "#644117";
